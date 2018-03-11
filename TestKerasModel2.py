748149beb20c39809793ee2c273e5a5a8011d6cf
MIZAR\JORIGGI00 <juan-manuel.origgi@pwc.com>
2018-03-11 01:51:34 +0100

    commit

--- TestKerasModel2.py
+++ TestKerasModel2.py
@@ -1,7 +1,6 @@
-#INPUT: [ [ [input at time1][time2][..][..][..] ] ]
-#LSTM Input Shape: 3D tensor with shape (batch_size, timesteps, input_dim)
+#LSTM INPUT ARRAY SHAPE: 3D tensor with shape (batch_size, timesteps, input_dim)
 ##Batchsize: numero di sample da considerare come chunk, quindi considero ad esmepio i primi 10 poi i successivi 10 riallenando la rete ogni volta.
-#LSTM(hidden_nodes, input_shape=(timesteps, input_dim)))
+#MODEL DECLARATION : LSTM(hidden_nodes, input_shape=(timesteps, input_dim)))
 ##Hidden_nodes = This is the number of neurons of the LSTM. If you have a higher number, the network gets more powerful.
 ##Timesteps = the number of timesteps you want to consider. E.g. if you want to classify a sentence, this would be the number of words in a sentence.
 ##Input_dim = the dimensions of your features/embeddings. E.g. a vector representation of the words in the sentence
@@ -12,16 +11,16 @@ import numpy as np
 
 #PREPARE TRAINING DATA
 def get_train():
-    data = np.array([0,1,2,3,4,5,6,7,8,9])
-    labels = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7],[7,8],[8,9],[9,10],[10,11]]
+    data = np.array([[0,1],[1,2],[2,3],[3,4],[4,5]])
+    labels = [[1,2],[2,3],[3,4],[4,5],[5,6]]
     X = data
     y = np.asarray(labels)
-    X = X.reshape((len(X),1, 1))
+    X = X.reshape((len(X),1, 2))
     return X, y
 
 #DEFINE MODEL
 model = Sequential()
-model.add(LSTM(10, input_shape=(1,1)))
+model.add(LSTM(10, input_shape=(1,2)))
 model.add(Dense(2, activation='linear'))
 
 #COMPILE MODEL
---
+++ TestKerasPredict2.py
@@ -0,0 +1,15 @@
+import numpy as np
+from keras.models import load_model
+
+#LOAD MODEL FROM FILE
+model = load_model('lstm_model.h5')
+
+#PREDICTION
+#test = array([5])
+test = np.array([[0,1],[1,2],[2,3],[3,4],[4,5]])
+test = test.reshape((len(test), 1, 1))
+
+#yhat = model.predict(X, verbose=0)
+yhat = model.predict(test, verbose=0)
+
+print('Result: ',yhat)
