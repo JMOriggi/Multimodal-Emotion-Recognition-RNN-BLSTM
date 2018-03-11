b21ebd960ca018b5a878541f4dc8f14d86f768cc
MIZAR\JORIGGI00 <juan-manuel.origgi@pwc.com>
2018-03-11 02:02:05 +0100

    commit before plane. new keras V2 V3

--- TestKerasModel3.py
+++ TestKerasModel3.py
@@ -1,20 +1,20 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from numpy import array
+import numpy as np
 
 #PREPARE TRAINING DATA
 def get_train():
-    data = array(range(0,25))
-    labels = array(range(1,26))
+    data = np.array([[0,1],[1,2],[2,3],[3,4],[4,5]])
+    labels = [[2],[3],[4],[5],[6]]
     X = data
-    y = labels
-    X = X.reshape((len(X), 1, 1))
+    y = np.asarray(labels)
+    X = X.reshape((len(X),1, 2))
     return X, y
 
 #DEFINE MODEL
 model = Sequential()
-model.add(LSTM(10, input_shape=(1,1)))
+model.add(LSTM(10, input_shape=(1,2)))
 model.add(Dense(1, activation='linear'))
 
 #COMPILE MODEL
@@ -30,8 +30,5 @@ model.fit(X, y, epochs=5000, shuffle=False, verbose=2)
 
 #SAVE MODEL AND WEIGHTS AFTER TRAINING
 model.save('lstm_model.h5')
-
-
-
 
 
