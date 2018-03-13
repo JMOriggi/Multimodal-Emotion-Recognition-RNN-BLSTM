542992d5c195964e1e05b47a15266b161b3395fb
MIZAR\JORIGGI00 <juan-manuel.origgi@pwc.com>
2018-03-13 10:32:45 +0100

    commit

--- TestKerasModel2.py
+++ TestKerasModel2.py
@@ -1,9 +1,4 @@
-#LSTM INPUT ARRAY SHAPE: 3D tensor with shape (batch_size, timesteps, input_dim)
-##Batchsize: numero di sample da considerare come chunk, quindi considero ad esmepio i primi 10 poi i successivi 10 riallenando la rete ogni volta.
-#MODEL DECLARATION : LSTM(hidden_nodes, input_shape=(timesteps, input_dim)))
-##Hidden_nodes = This is the number of neurons of the LSTM. If you have a higher number, the network gets more powerful.
-##Timesteps = the number of timesteps you want to consider. E.g. if you want to classify a sentence, this would be the number of words in a sentence.
-##Input_dim = the dimensions of your features/embeddings. E.g. a vector representation of the words in the sentence
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
--- TestKerasModel3.py
+++ TestKerasModel3.py
@@ -1,3 +1,13 @@
+#LSTM INPUT ARRAY SHAPE: 3D tensor with shape (batch_size, timesteps, input_dim)
+##Batchsize: numero di sample da considerare come chunk, quindi considero ad esmepio i primi 10 poi i successivi 10 riallenando la rete ogni volta.
+#MODEL DECLARATION : LSTM(hidden_nodes, input_shape=(timesteps, input_dim)))
+##Hidden_nodes = This is the number of neurons of the LSTM. If you have a higher number, the network gets more powerful.
+##Timesteps = the number of timesteps you want to consider. E.g. if you want to classify a sentence, this would be the number of words in a sentence.
+##Input_dim = the dimensions of your features/embeddings. E.g. a vector representation of the words in the sentence
+#INPUT MATRIX SHAPE FOR KERAS: [A B C ...]--> struttura 1 input con valori ABC...
+#                                [[A B C ..]]--> struttura 1 sample
+#                                [ [[a b c]] [[d e f]] ]--> struttura array X con 2 sample 
+#OUTPUT MATRIX SHAPE FOR KERAS: [ [1] [2] [3] ]--> output shape with 3 sample output.
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
