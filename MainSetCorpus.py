103253970af41bfd793d9a66a306272e2c0174e3
Manuel <Manuel@DESKTOP-J2KQ82T.lan>
2018-03-21 19:59:51 +0100

    COM

---
+++ MainSetCorpus.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+import DataTrainingUtils as trainData
+import AudioUtils as aud
+import NeuralNetworkUtils as nn
+
+#SET ROOTS
+#mainRoot = os.path.normpath(r'C:\Users\JORIGGI00\Documents\MyDOCs\Corpus_lav2')
+mainRootTraining = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test_Training')
+mainRootTest = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test_Test')
+
+#CREATE TRAINING OUTPUT DATA FILE
+trainData.setDataCorpus(mainRootTraining)
+
+#CREATE TEST OUTPUT DATA FILE
+trainData.setDataCorpus(mainRootTest)
