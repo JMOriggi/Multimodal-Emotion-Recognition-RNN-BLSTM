a7d256769c38014023916112d14c3c2b247527bc
MIZAR\JORIGGI00 <juan-manuel.origgi@pwc.com>
2018-03-22 12:48:10 +0100

    com condition

--- MainTraining.py
+++ MainTraining.py
@@ -28,45 +28,46 @@ for session in sessDirList:
         for Afile in audlist:
             audioFilePath = os.path.join(currentAudioGroupPath, Afile) 
             
-            #READ AUDIO FILE: tranform it in a redable array in spectrum
-            arrayAudio, sampleRate = aud.getArrayFromAudio(audioFilePath)
-            allFrameFFT = aud.getFreqArray(arrayAudio, sampleRate)
-            
-            #READ TRAINING OUTPUT DATA: corresponding to that audio file
+            #READ TRAINING OUTPUT DATA: corresponding to the current audio file; if emotion xxx, neu or other don't consider it for training
             y_code, output, emo, val, text = trainData.getOutputDataFromAudio(Afile.split('.')[0], mainRoot)
             '''print('---Coresponding output for Audio ', Afile)
             print('Name: ',output.split(';')[0],'\nEmotion: ',emo,'\nValence: ',val,'\nTranscription: ',text,'Emo Label code: ', y_code, '\n')'''
             
-            #BUILD THE INPUT TRAINING ARRAY: dim = (#audiofile, #ofFftPerformed, fftWindowSize)
-            TInArray.append(allFrameFFT)
-            
-            #BUILD THE OUTPUT TRAINING ARRAY: dim = (#audiofile, outlabelArray)
-            TOutArray.append(y_code)
-            
-            '''print('\n')
-            print('TInArray number of audio file: ', len(TInArray))
-            print('TInArray number of timestep (number of FFT window): ', len(TInArray[0]))
-            print('TInArray number of freq considered (value is the amplitude in db for each one): ', len(TInArray[0][0]))
-            print('TOutArray number of audio file: ', len(TOutArray))
-            print('TOutArray number of output label for each file: ', len(TOutArray[0]))
-            print('\n')'''
-            
-            #FEED THE NN: done for 1 session at time, because the groupped audio file array contains only one session files
-            if flag > 0:
-                modelRNN = load_model('RNN_Model_saved.h5')    
-                model = nn.RNNModel(modelRNN, TInArray, TOutArray)
-            else:
-                print('CREATE NEW MODEL FILE FOR SAVE\n')
-                modelRNN = ''
-                model = nn.RNNModel(modelRNN, TInArray, TOutArray)
-                flag +=1
+            if y_code != [0,0,0,0,0,0,1]:
+                #READ AUDIO FILE: tranform it in a redable array in spectrum
+                arrayAudio, sampleRate = aud.getArrayFromAudio(audioFilePath)
+                allFrameFFT = aud.getFreqArray(arrayAudio, sampleRate)
                 
-            #SAVE MODEL AND WEIGHTS AFTER TRAINING
-            model.save('RNN_Model_saved.h5', overwrite=True)
-            
-            #RESET ARRAY: se non viene fatto accumulo in tin e tout tutti i file audio (risultato finale voluto, eseguendo la RNN all'uscita di tutto il ciclo)
-            TInArray = []    
-            TOutArray = []
+                #BUILD THE INPUT TRAINING ARRAY: dim = (#audiofile, #ofFftPerformed, fftWindowSize)
+                TInArray.append(allFrameFFT)
+                
+                #BUILD THE OUTPUT TRAINING ARRAY: dim = (#audiofile, outlabelArray)
+                TOutArray.append(y_code)
+                
+                '''print('\n')
+                print('TInArray number of audio file: ', len(TInArray))
+                print('TInArray number of timestep (number of FFT window): ', len(TInArray[0]))
+                print('TInArray number of freq considered (value is the amplitude in db for each one): ', len(TInArray[0][0]))
+                print('TOutArray number of audio file: ', len(TOutArray))
+                print('TOutArray number of output label for each file: ', len(TOutArray[0]))
+                print('\n')'''
+                
+                #FEED THE NN: done for 1 session at time, because the groupped audio file array contains only one session files
+                if flag > 0:
+                    modelRNN = load_model('RNN_Model_saved.h5')    
+                    model = nn.RNNModel(modelRNN, TInArray, TOutArray)
+                else:
+                    print('CREATE NEW MODEL FILE FOR SAVE\n')
+                    modelRNN = ''
+                    model = nn.RNNModel(modelRNN, TInArray, TOutArray)
+                    flag +=1
+                    
+                #SAVE MODEL AND WEIGHTS AFTER TRAINING
+                model.save('RNN_Model_saved.h5', overwrite=True)
+                
+                #RESET ARRAY: se non viene fatto accumulo in tin e tout tutti i file audio (risultato finale voluto, eseguendo la RNN all'uscita di tutto il ciclo)
+                TInArray = []    
+                TOutArray = []
             
 print('END OF TRAINING')  
 
