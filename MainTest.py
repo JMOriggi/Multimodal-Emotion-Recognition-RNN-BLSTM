22ae061f9d2ec964437e1b34477a8d85709468de
MIZAR\JORIGGI00 <juan-manuel.origgi@pwc.com>
2018-03-26 15:12:39 +0200

    c

--- MainTest.py
+++ MainTest.py
@@ -5,6 +5,7 @@ trainData.moveCopyAudioFiles(mainRoot)'''
 
 import numpy as np
 import csv
+from numpy import genfromtxt
 
 a = [[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]]
 x = []
@@ -14,17 +15,19 @@ x.append(np.asarray(a))'''
 x.append(a)
 x.append(a)
 x.append(a)
-print(x)
+print('prepared array: ',x)
 
 with open("output.csv", "w", newline='') as f:
     writer = csv.writer(f)
     writer.writerows(x)
 f.close()
 
-datareader = csv.reader(open('output.csv', 'r'))
+'''datareader = csv.reader(open('output.csv', 'r'))
 data = []
 for row in datareader:
-    data.append([val for val in row])
+    data.append([np.array(val) for val in row])
 print(data)    
-Y = np.array([np.array(xi) for xi in data])
-print(Y)
+Y = np.array([np.array(xi) for xi in data])'''
+
+Y = genfromtxt('output.csv', delimiter=',')
+print('Readed val: ',Y)
