cf9e0d3c5b818f08a467111662d028e51f25e45d
MIZAR\JORIGGI00 <juan-manuel.origgi@pwc.com>
2018-03-26 12:54:28 +0200

    c

--- MainTest.py
+++ MainTest.py
@@ -1,24 +1,30 @@
-import numpy as np
-
-a = np.array([[1,2,3]])
-b = np.array([[4,5,6]])
-print(a)
-print(b)
-str1 = str(a[0])
-str2 = ''.join(str(e) for e in b)
-print(str1)
-print(str2)
-audioname = 'NameOfTheFile'
-r=[]
-r=audioname+',EXP:'+str1+',AUD:'+str2+',TEXT:'+str2+'\n'
-print(r)
-
-file = open('TEst.txt','w')
-file.writelines(r)
-file.writelines(r)
-file.close()
-
 '''import os
 import DataTrainingUtils as trainData
 mainRoot = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test_Training')
 trainData.moveCopyAudioFiles(mainRoot)'''
+
+import numpy as np
+import csv
+
+a = [[1,2,3,4,5,6,7],[8,9,10,11,12,13,14]]
+x = []
+'''x.append(np.asarray(a))
+x.append(np.asarray(a))
+x.append(np.asarray(a))'''
+x.append(a)
+x.append(a)
+x.append(a)
+print(x)
+
+with open("output.csv", "w", newline='') as f:
+    writer = csv.writer(f)
+    writer.writerows(x)
+f.close()
+
+datareader = csv.reader(open('output.csv', 'r'))
+data = []
+for row in datareader:
+    data.append([val for val in row])
+print(data)    
+Y = np.array([np.array(xi) for xi in data])
+print(Y)
