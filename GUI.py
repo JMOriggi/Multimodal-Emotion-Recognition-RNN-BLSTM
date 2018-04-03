0eb57a4605e36af87a8eb2835926ae62c3a472c4
Manuel <Manuel@DESKTOP-J2KQ82T.lan>
2018-04-04 00:19:44 +0200

    commit

--- GUI.py
+++ GUI.py
@@ -1,137 +1,172 @@
 import sys
 import os
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QLabel, QCheckBox, QWidget, QFileDialog, QPushButton
-from PyQt5.QtCore import QSize    
+from PyQt5.QtWidgets import QMainWindow, QLabel, QCheckBox, QWidget, QFileDialog, QPushButton, QTextEdit
+from PyQt5.QtCore import QSize 
+from PyQt5.QtGui import QFont 
+import MainSetCorpus as SetCorp  
 
-#DEFINE GLOBAL VARIABLE ROOT 
-MainRoot = ''
-mainRootT = ''
-AudioTextFlag = 0 #2 for both, 1 for only text, 0 for only audio
+
      
 #CLASS FOR WINDOW CREATION     
 class Window(QMainWindow):
+    
+    #DEFINE GLOBAL VARIABLE ROOT 
+    MainRoot = ''
+    MainRootT = ''
+    AudioTextFlag = 0 #2 for both, 1 for only text, 0 for only audio
+    
     def __init__(self):
         QMainWindow.__init__(self)
  
         #WINDOW SIZE
-        self.setGeometry(600, 200, 500, 300)
-        #self.setMinimumSize(QSize(140, 40))    
+        self.setGeometry(600, 200, 500, 300) 
         self.setWindowTitle("Emotional Neural Network") 
         
-        #TRAINING1 CHECKBOX
+        #LABELS
+        myFont = QFont()
+        myFont.setBold(True)
+        self.l1 = QLabel(self)
+        self.l1.setText('Select Corpus:')
+        self.l1.setFont(myFont)
+        self.l1.move(10,0)
+        self.l2 = QLabel(self)
+        self.l2.setText('Select NN type:')
+        self.l2.setFont(myFont)
+        self.l2.move(10,80)
+        
+        #CHECKBOXS 
         self.b1 = QCheckBox("Original Corpus",self)
         self.b1.stateChanged.connect(self.clickBoxOriginal)
         self.b1.move(20,20)
-        #TRAINING2 CHECKBOX
         self.b2 = QCheckBox("Fake Corpus",self)
         self.b2.stateChanged.connect(self.clickBoxFake)
-        self.b2.move(20,50)
-        #TRAINING3 CHECKBOX
+        self.b2.move(20,40)
         self.b3 = QCheckBox("Lav",self)
         self.b3.stateChanged.connect(self.clickBoxLav)
-        self.b3.move(20,80)
-        #AUDIO TRAINING
+        self.b3.move(20,60)
         self.b4 = QCheckBox("Audio",self)
         self.b4.stateChanged.connect(self.clickBoxA)
-        self.b4.move(20,130)
-        #TEXT TRAINING
+        self.b4.move(20,100)
         self.b5 = QCheckBox("Text",self)
         self.b5.stateChanged.connect(self.clickBoxT)
-        self.b5.move(20,150)
-        #AUDIO+TEXT TRAINING
+        self.b5.move(20,120)
         self.b6 = QCheckBox("Audio+Text",self)
         self.b6.stateChanged.connect(self.clickBoxAT)
-        self.b6.move(20,170)
+        self.b6.move(20,140)
         
-        #BUTTON SET CORPUS
-        self.btn_runTest = QPushButton("SET CORPUS", self)
-        self.btn_runTest.clicked.connect(self.runSetCorpus)
-        self.btn_runTest.setFixedSize(100,50)
-        self.btn_runTest.move(340,20)
-        
-        #BUTTON TRAINING
-        self.btn_runTest = QPushButton("RUN TRAINING", self)
-        self.btn_runTest.clicked.connect(self.runTraining)
-        self.btn_runTest.setFixedSize(100,50)
-        self.btn_runTest.move(340,100)
-        
-        #BUTTON TEST
+        #BUTTONS
+        self.btn_setCorpus = QPushButton("SET CORPUS", self)
+        self.btn_setCorpus.clicked.connect(self.runSetCorpus)
+        self.btn_setCorpus.setFixedSize(100,30)
+        self.btn_setCorpus.move(340,20)
+        self.btn_runTraining = QPushButton("RUN TRAINING", self)
+        self.btn_runTraining.clicked.connect(self.runTraining)
+        self.btn_runTraining.setFixedSize(100,30)
+        self.btn_runTraining.move(340,60)
         self.btn_runTest = QPushButton("RUN TEST", self)
         self.btn_runTest.clicked.connect(self.runTest)
-        self.btn_runTest.setFixedSize(100,50)
-        self.btn_runTest.move(340,180)
-              
+        self.btn_runTest.setFixedSize(100,30)
+        self.btn_runTest.move(340,100)
+        
+        #CONSOLE LOG
+        self.logOutput = QTextEdit(self)
+        self.logOutput.setReadOnly(True)
+        self.logOutput.setLineWrapMode(QTextEdit.NoWrap)
+        font = self.logOutput.font()
+        font.setFamily("Courier")
+        font.setPointSize(10)
+        self.logOutput.setFixedSize(460,90)
+        self.logOutput.move(20,200)
+        #Button clear log
+        self.btn_clear = QPushButton("Clear Logs", self)
+        self.btn_clear.clicked.connect(self.clearLog)
+        self.btn_clear.setFixedSize(100,25)
+        self.btn_clear.move(380,170)   
+    
+    #PRINT LOG
+    def printLog(self, text):
+        self.logOutput.insertPlainText(text+'\n')
+        sb = self.logOutput.verticalScrollBar()
+        sb.setValue(sb.maximum()) 
+    def clearLog(self):
+        self.logOutput.clear()    
+        
+    #CHECKBOX METHODS          
     def clickBoxOriginal(self, state):
         if state == QtCore.Qt.Checked:
-            MainRoot = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Training')
-            MainRootT = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test')
-            print('Checked clickBoxOriginal')
+            self.MainRoot = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Training')
+            self.MainRootT = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test')
+            txt = 'Checked on roots: '+self.MainRoot+', AND, '+self.MainRootT
+            self.printLog(txt)
         else:
-            MainRoot = ''
-            print('Unchecked clickBoxOriginal')
-            
+            self.MainRoot = ''
+            self.printLog('Unchecked clickBoxOriginal') 
     def clickBoxFake(self, state):
         if state == QtCore.Qt.Checked:
-            MainRoot = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test_Training')
-            MainRootT = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test_Training')
-            print('Checked clickBoxFake')
+            self.MainRoot = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test_Training')
+            self.MainRootT = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test_Training')
+            txt = 'Checked on roots: '+self.MainRoot+', AND, '+self.MainRootT
+            self.printLog(txt)
         else:
-            MainRoot = ''
-            print('Unchecked clickBoxFake')
-    
+            self.MainRoot = ''
+            self.printLog('Unchecked clickBoxFake') 
     def clickBoxLav(self, state):
         if state == QtCore.Qt.Checked:
-            MainRoot = os.path.normpath(r'C:\Users\JORIGGI00\Documents\MyDOCs\Corpus_Test_Training')
-            MainRootT = os.path.normpath(r'C:\Users\JORIGGI00\Documents\MyDOCs\Corpus_Test_Training')
-            print('Checked clickBoxLav')
+            self.MainRoot = os.path.normpath(r'C:\Users\JORIGGI00\Documents\MyDOCs\Corpus_Test_Training')
+            self.MainRootT = os.path.normpath(r'C:\Users\JORIGGI00\Documents\MyDOCs\Corpus_Test_Training')
+            txt = 'Checked on roots: '+self.MainRoot+', AND, '+self.MainRootT
+            self.printLog(txt)
         else:
-            MainRoot = ''
-            print('Unchecked clickBoxLav')                
-    
+            self.MainRoot = ''
+            self.printLog('Unchecked clickBoxLav')                
     def clickBoxA(self, state):
         if state == QtCore.Qt.Checked:
             AudioTextFlag = 0
-            print('Checked clickBoxAT: ',AudioTextFlag)
+            txt = 'Checked only audio'
+            self.printLog(txt)
         else:
             AudioTextFlag = 0
-            print('Checked clickBoxAT: ',AudioTextFlag)
-            
     def clickBoxT(self, state):
         if state == QtCore.Qt.Checked:
             AudioTextFlag = 1
-            print('Checked clickBoxAT: ',AudioTextFlag)
+            txt = 'Checked only text'
+            self.printLog(txt)
         else:
-            AudioTextFlag = 0
-            print('Checked clickBoxAT: ',AudioTextFlag)  
-            
+            AudioTextFlag = 0  
     def clickBoxAT(self, state):
         if state == QtCore.Qt.Checked:
             AudioTextFlag = 2
-            print('Checked clickBoxAT: ',AudioTextFlag)
+            txt = 'Checked audio and text'
+            self.printLog(txt)
         else:
-            AudioTextFlag = 0
-            print('Checked clickBoxAT: ',AudioTextFlag)               
+            AudioTextFlag = 0               
  
- 
+    #BUTTONS METHODS 
     def runSetCorpus(self, w):
-        if MainRoot == '':
-            print ('Select one Root checkbox')
+        if self.MainRoot == '':
+            self.printLog('Select one Root checkbox')
         else:
-            print ('runSetCorpus')
+            txt = 'Set Corpus on roots: '+self.MainRoot+', AND, '+self.MainRootT
+            self.printLog(txt)
+            SetCorp.mainSetCorpus(self.MainRoot,self.MainRootT)
+            self.printLog('END of set Corpus')
             
     def runTraining(self, w):
-        if MainRoot == '':
-            print ('Select one Root checkbox')
+        if self.MainRoot == '':
+            self.printLog('Select one Root checkbox')
         else:
-            print ('runTraining') 
-        
+            txt = 'Run training with root: '+self.MainRoot
+            self.printLog(txt) 
+            self.printLog('END of Training') 
+            
     def runTest(self, w):
-        if MainRoot == '':
-            print ('Select one Root checkbox')
+        if self.MainRoot == '':
+            self.printLog('Select one Root checkbox')
         else:
-            print ('runTest')     
-
+            txt = 'Set Test with roots: '+self.MainRoot+', AND, '+self.MainRootT
+            self.printLog(txt) 
+            self.printLog('END of Test')     
             
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
--- MainSetCorpus.py
+++ MainSetCorpus.py
@@ -6,25 +6,24 @@ import NeuralNetworkUtils as nn
 
 #SET MAIN ROOT
 #mainRootTraining = os.path.normpath(r'C:\Users\JORIGGI00\Documents\MyDOCs\Corpus_Test_Training')
-mainRootTraining = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Training')
-mainRootTest = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test')
+#mainRootTraining = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Training')
+#mainRootTest = os.path.normpath('D:\DATA\POLIMI\----TESI-----\Corpus_Test')
 
-#TRAINING FOLDER
-#Move copy of audio files
-trainData.moveCopyAudioFiles(mainRootTraining)
-#Create training data file
-trainData.clusterData(mainRootTraining)
-trainData.encoder(mainRootTraining)
 
-#TEST FOLDER
-#Move copy of audio files
-trainData.moveCopyAudioFiles(mainRootTest)
-#Create training data file
-trainData.clusterData(mainRootTest)
-trainData.encoder(mainRootTest)
-
-'''#CREATE TRAINING OUTPUT DATA FILE
-trainData.setDataCorpus(mainRootTraining)
-trainData.setDataCorpus(mainRootTest)'''
-
-print('END OF SET CORPUS')
+def mainSetCorpus(mainRootTraining, mainRootTest):
+    
+    #TRAINING FOLDER
+    #Move copy of audio files
+    trainData.moveCopyAudioFiles(mainRootTraining)
+    #Create training data file
+    trainData.clusterData(mainRootTraining)
+    trainData.encoder(mainRootTraining)
+    
+    #TEST FOLDER
+    #Move copy of audio files
+    trainData.moveCopyAudioFiles(mainRootTest)
+    #Create training data file
+    trainData.clusterData(mainRootTest)
+    trainData.encoder(mainRootTest)
+    
+    print('END OF SET CORPUS')
