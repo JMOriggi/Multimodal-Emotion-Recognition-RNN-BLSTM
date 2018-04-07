f962e8e5fd842086578e2dfb6f375d488703b401
Manuel <Manuel@DESKTOP-J2KQ82T.lan>
2018-04-07 23:35:48 +0200

    c

--- Utils_create_audio_feature.py
+++ Utils_create_audio_feature.py
@@ -8,7 +8,11 @@ from scipy.io.wavfile import read
 import matplotlib.pyplot as plt 
 import numpy as np 
 import librosa
-
+import pyacoustics
+from pyacoustics import intensity_and_pitch
+from praatio import tgio
+from praatio import praatio_scripts
+from praatio import pitch_and_intensity
 
 def readWav(audioFilePath):
     #READ THE WAV FILE
@@ -17,39 +21,34 @@ def readWav(audioFilePath):
     stereoAudio = inputAudio[1]
     #TRASFORM IN MONO: no need because audio already in mono
     monoAudio = stereoAudio
-    monoAudio = np.array(monoAudio, dtype=float)
+    monoAudio = np.array(monoAudio, dtype=float) #type float necessary
     return monoAudio, sampleRate
 
-def buildFeature(monoAudio, sampleRate): 
-    #COMPUTE SPECTROGRAM: NFFT=how many sample in one chunk, for Fs16000 chunks20ms-->32
-    fft, freqsBins, timeBins, im = plt.specgram(monoAudio, Fs=sampleRate, NFFT=320, cmap=plt.get_cmap('autumn_r'))
-    
-    #RESHAPE FREQ ARRAY: row=timestep collumns=freq values
-    maxFftValues = 50 #len(fft) #voce si estende 80Hz-1500Hz mentre fft va da 0-8000 (con 50 ho fino a 2450Hz)
-    X = np.full((len(fft[0]), maxFftValues), 0)
-    i = 0
-    while i < maxFftValues:
-        y = 0
-        while y < len(fft[0]):
-            X[y][i] = fft[i][y]
-            y+=1
-        i+=1
+def buildFeatures(monoAudio, sampleRate):
     
     #n_fft is the window of 25ms = 400samples e hop_lenght is the step between one frame and another
     mfcc = librosa.feature.mfcc(monoAudio, sampleRate, n_mfcc=40, hop_length=int(0.010*sampleRate), n_fft=int(0.020*sampleRate))
-    print(mfcc)
-    print(mfcc.dtype)
+    mfcc_delta = librosa.feature.delta(mfcc)
+    mfcc_delta_delta = librosa.feature.delta(mfcc, order=2)
     print(mfcc.shape)
+    print(mfcc_delta.shape)
+    print(mfcc_delta_delta.shape)
     #Transpose because librosa gives me the freq in the row but i want them in the collums
     mfcc = mfcc.T
-    print(mfcc)
+    mfcc_delta = mfcc_delta.T
+    mfcc_delta_delta = mfcc_delta_delta.T
     print(mfcc.shape)
+    print(mfcc_delta.shape)
+    print(mfcc_delta_delta.shape)
+    
+    pitch_and_intensity.extractPitch(wavFN, outputFN, praatEXE, minPitch, maxPitch, sampleStep, silenceThreshold, forceRegenerate, undefinedValue, medianFilterWindowSize, pitchQuadInterp)
+    pitch_and_intensity.extractIntensity(inputFN, outputFN, praatEXE, minPitch, sampleStep, forceRegenerate, undefinedValue)
     
     #to concatenate row per row 
-    x = np.hstack((mfcc,mfcc,mfcc))
-    print(x.shape)
+    X = np.hstack((mfcc, mfcc_delta, mfcc_delta_delta))
+    print(X.shape)
     
-    return mfcc
+    return X
 
 def saveFeaturecsv(currentFileFeatures, csvOutputFilePath):
     csvOutputFilePath = os.path.join(csvOutputFilePath + '.csv')
@@ -76,7 +75,7 @@ def buildAudioCsv(mainRoot, audioDirectoryPath, out_audio_feature_path):
         
         arrayAudio, sampleRate = readWav(audioFilePath)
         
-        currentFileFeatures = buildFeature(arrayAudio, sampleRate)
+        currentFileFeatures = buildFeatures(arrayAudio, sampleRate)
         
         saveFeaturecsv(currentFileFeatures, csvOutputFilePath)
         
