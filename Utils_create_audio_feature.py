97c12cd9c5aed316960e5d464f1995862e6c4a66
Manuel <Manuel@DESKTOP-J2KQ82T.lan>
2018-04-08 17:25:11 +0200

    c

--- Utils_create_audio_feature.py
+++ Utils_create_audio_feature.py
@@ -13,47 +13,107 @@ from pyacoustics import intensity_and_pitch
 from praatio import tgio
 from praatio import praatio_scripts
 from praatio import pitch_and_intensity
+from Signal_Analysis import features
+from numpy.fft import rfft, irfft
+from numpy import argmax, sqrt, mean, diff, log
+from matplotlib.mlab import find
+from scipy.signal import blackmanharris, fftconvolve
+from time import time
+import parabolic
+
+def estimate_pitch(monoAudio, sampleRate, window, hop):
+    
+    pitch , mag = librosa.core.piptrack(monoAudio, sampleRate, n_fft=window, hop_length=hop, fmin=50, fmax=1500)
+    pitch = pitch.T
+    mag = mag.T
+    print(len(pitch))
+    f0 = []
+    i = 0
+    while i < len(pitch):
+        index = mag[i, :].argmax()
+        f0.append(pitch[i, index])
+        i += 1
+    f0 = np.asarray(f0)
+    f0 = f0.reshape((len(f0),1))
+    
+    return f0
 
 def readWav(audioFilePath):
-    #READ THE WAV FILE
+    
     inputAudio = read(audioFilePath)
     sampleRate = inputAudio[0]
-    stereoAudio = inputAudio[1]
-    #TRASFORM IN MONO: no need because audio already in mono
-    monoAudio = stereoAudio
+    monoAudio = inputAudio[1]
     monoAudio = np.array(monoAudio, dtype=float) #type float necessary
+    
     return monoAudio, sampleRate
 
 def buildFeatures(monoAudio, sampleRate):
+    #TODO: togliere primo e ultimo frame per tutte le feature
     
+    window = int(0.020*sampleRate)
+    hop = int(0.010*sampleRate)
+    
+    #PITCH:
+    pitch = estimate_pitch(monoAudio, sampleRate, window, hop)
+    print(pitch)
+    #pitch_delta = librosa.feature.delta(pitch, width=3)
+    #pitch_delta_delta = librosa.feature.delta(pitch, width=3, order=2)
+    
+    '''pitch = pitch.T
+    mag = mag.T
+    print(pitch.shape)
+    print(mag.shape)
+    print(mag[0])
+    print(pitch[0])'''
+    
+    #ENERGY
+    energy = librosa.feature.rmse(y=monoAudio, frame_length=window, hop_length=hop)
+    energy_delta = librosa.feature.delta(energy, width=3)
+    energy_delta_delta = librosa.feature.delta(energy, width=3, order=2)
+    
+    #MFCC
     #n_fft is the window of 25ms = 400samples e hop_lenght is the step between one frame and another
-    mfcc = librosa.feature.mfcc(monoAudio, sampleRate, n_mfcc=40, hop_length=int(0.010*sampleRate), n_fft=int(0.020*sampleRate))
-    mfcc_delta = librosa.feature.delta(mfcc)
-    mfcc_delta_delta = librosa.feature.delta(mfcc, order=2)
-    '''print(mfcc.shape)
-    print(mfcc_delta.shape)
-    print(mfcc_delta_delta.shape)'''
+    #For the deltas important to set width=3 to compute derivative of each sample with previous and next sample.
+    mfcc = librosa.feature.mfcc(monoAudio, sampleRate, n_mfcc=40, n_fft=window, hop_length=hop)
+    mfcc_delta = librosa.feature.delta(mfcc, width=3)
+    mfcc_delta_delta = librosa.feature.delta(mfcc, width=3, order=2)
+    
+    #MFCC ENERGY
+    mfcc_energy = librosa.feature.rmse(S=mfcc, frame_length=window, hop_length=hop)
+    mfcc_energy_delta = librosa.feature.rmse(S=mfcc_delta, frame_length=window, hop_length=hop)
+    mfcc_energy_delta_delta = librosa.feature.rmse(S=mfcc_delta_delta, frame_length=window, hop_length=hop)
+    
     #Transpose because librosa gives me the freq in the row but i want them in the collums
+    energy = energy.T
+    energy_delta = energy_delta.T
+    energy_delta_delta = energy_delta_delta.T
     mfcc = mfcc.T
     mfcc_delta = mfcc_delta.T
     mfcc_delta_delta = mfcc_delta_delta.T
-    '''print(mfcc.shape)
-    print(mfcc_delta.shape)
-    print(mfcc_delta_delta.shape)'''
+    mfcc_energy = mfcc_energy.T
+    mfcc_energy_delta = mfcc_energy_delta.T
+    mfcc_energy_delta_delta = mfcc_energy_delta_delta.T
+    '''pitch = pitch.T
+    pitch_delta = pitch_delta.T
+    pitch_delta_delta = pitch_delta_delta.T'''
+    '''print('mfcc.shape: ', mfcc.shape)
+    print('mfcc_delta.shape: ', mfcc_delta.shape)
+    print('mfcc_delta_delta.shape: ', mfcc_delta_delta.shape)
+    print('energy.shape: ', energy.shape)
+    print('energy_delta.shape: ', energy_delta.shape)
+    print('energy_delta_delta.shape: ', energy_delta_delta.shape)
+    print('mfcc_energy.shape: ', mfcc_energy.shape)
+    print('mfcc_energy_delta.shape: ', mfcc_energy_delta.shape)
+    print('mfcc_energy_delta_delta.shape: ', mfcc_energy_delta_delta.shape)'''
+    '''print('pitch.shape: ', pitch.shape)
+    print('pitch_delta.shape: ', pitch_delta.shape)
+    print('pitch_delta_delta.shape: ', pitch_delta_delta.shape)'''
     
-    #pitch_and_intensity.extractPitch(wavFN, outputFN, praatEXE, minPitch, maxPitch, sampleStep, silenceThreshold, forceRegenerate, undefinedValue, medianFilterWindowSize, pitchQuadInterp)
-    #pitch_and_intensity.extractIntensity(inputFN, outputFN, praatEXE, minPitch, sampleStep, forceRegenerate, undefinedValue)
-    pitch , mag = librosa.core.piptrack(monoAudio, sampleRate, hop_length=int(0.010*sampleRate), n_fft=int(0.020*sampleRate),fmin=0, fmax=10)
-    pitch = mfcc.T
-    mag = mfcc.T
-    print(pitch.shape)
-    print(pitch)
-    #print(mag)
-    #print(mag.shape)
     
-    #to concatenate row per row 
-    X = np.hstack((mfcc, mfcc_delta, mfcc_delta_delta))
-    #print(X.shape)
+    
+    #CONCATENATE FEATURES PER ROWS
+    X = np.hstack((energy, energy_delta, energy_delta_delta, mfcc, mfcc_delta, mfcc_delta_delta, mfcc_energy, mfcc_energy_delta, mfcc_energy_delta_delta))
+    print(X.shape)
     
     return X
 
