146ac37c9d03b131de6cd4d1d2f086c38153234b
Manuel <Manuel@DESKTOP-J2KQ82T.lan>
2018-04-09 00:35:43 +0200

    c

---
+++ Utils_create_emotion_labels.py
@@ -0,0 +1,75 @@
+import os
+import csv
+import shutil
+import numpy as np
+
+
+if __name__ == '__main__':
+    
+    main_root = os.path.normpath(r'D:\DATA\POLIMI\----TESI-----\NewCorpus')
+    index_file_path =  os.path.join(main_root+'\AllData.txt')
+    out_csv_labels_path = os.path.join(main_root+'\LablesEmotion') 
+    
+    #READ THE FILE AND BUILD ARRAYS
+    #Audio File Names
+    with open(index_file_path, 'r') as AllDatafile:
+        X = [line.strip() for line in AllDatafile] 
+        arrayFileName = [line.split(';')[0] for line in X] 
+    AllDatafile.close()
+    #Emotion Labels
+    with open(index_file_path, 'r') as AllDatafile:
+        Y = [line.strip() for line in AllDatafile] 
+        arrayEmoLabel = [line.split(';')[1] for line in Y]
+    AllDatafile.close()  
+    #Transcriptions   
+    with open(index_file_path, 'r') as AllDatafile:
+        Z = [line.strip() for line in AllDatafile] 
+        arrayText = [line.split(';')[2] for line in Z]
+    AllDatafile.close()
+    
+    #ENCODE EMOTIONS
+    i = 0
+    emoEncoded = []
+    while i < len(arrayEmoLabel):
+        emoLabel = arrayEmoLabel[i]
+        if  emoLabel == 'exc' or emoLabel == 'hap': 
+            code = [1,0,0,0,0,0,0] #JOY
+        if  emoLabel == 'ang' or emoLabel == 'fru':    
+            code = [0,1,0,0,0,0,0] #ANG
+        if  emoLabel == 'dis': 
+            code = [0,0,1,0,0,0,0] 
+        if  emoLabel == 'sad': 
+            code = [0,0,0,1,0,0,0] 
+        if  emoLabel == 'sur': 
+            code = [0,0,0,0,1,0,0] 
+        if  emoLabel == 'fea': 
+            code = [0,0,0,0,0,1,0]  
+        if  emoLabel == 'neu': 
+            code = [0,0,0,0,0,0,1]
+        if  emoLabel == 'other' or emoLabel == 'xxx': 
+            code = [0,0,0,0,0,0,2]  #NOT CLASSIFIED
+        emoEncoded.append(code)
+        i += 1
+    
+    #WRITE CSV FILE
+    #Emotion Labels
+    i = 0
+    while i < len(emoEncoded):
+        #CREATE OUTPUTS DATA FILE: remove if it already exist and recreate it new
+        out_current_file = arrayFileName[i] + '.csv'
+        out_current_file = os.path.join(out_csv_labels_path, out_current_file) 
+        print(out_current_file)
+        try:
+            os.remove(out_current_file)
+        except OSError:
+            pass
+        
+        with open(out_current_file, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(emoEncoded[i])
+        csvfile.close()
+        i += 1
+    
+    print('END')
+    
+    
--- Utils_create_text_features.py
+++ Utils_create_text_features.py
@@ -5,10 +5,6 @@ import gensim
 import numpy as np
 import tensorflow as tf
 
-mainRoot = 'D:\DATA\POLIMI\----TESI-----\NewCorpus' #Cartella madre
-text_path = os.path.join(mainRoot+'\allData.txt') #file indice dei dati: nomeFile;emo;transcrizione
-outfile_Path_Tfr = '/Users/Mauro/Desktop/root_path_for_doubled_dataset/out/text/TestSet/Smoothed.tfrecords'
-
 
 def write_to_tfrecord(label, binary_text, writer):
     example = tf.train.Example(features=tf.train.Features(feature={
@@ -19,77 +15,50 @@ def write_to_tfrecord(label, binary_text, writer):
     writer.write(example.SerializeToString())
 
 
-def get_text_bynary(text, label, model):
+def get_text_bynary(text, model):
     counter = 0
     translated_text = []
-    lab = []
     notfounded = []
 
     for word in text:
-        if word == 'è':
-            word = word.replace('è', "e'")
-        counter += 1
+        print(word)
         try:
             translated_text.append(model.wv[word])
         except KeyError:
             translated_text.append(model.wv['unknown'])
             notfounded.append(word)
 
-    lab.append(label)
     text = np.array(translated_text, np.float32)
-    label = np.array(lab, np.float32)
-    return text.tobytes(), label.tobytes(), notfounded, counter
-
-
-def make_label(file):
-    (head, _) = os.path.split(file)
-    (_, label) = os.path.split(head)
-
-    print(label)
-
-    # 001, questions, 010 exclamations, 100 statements
-    if label == 'Questions':
-        one_hot_lab = [0, 0, 1]
-    elif label == 'Statements':
-        one_hot_lab = [1, 0, 0]
-    elif label == 'Exclamations':
-        one_hot_lab = [0, 1, 0]
-    else:
-        raise ("the fuck is goin' on")
-
-    return (one_hot_lab, label)
+    return text.tobytes(), notfounded, counter
 
 
 if __name__ == '__main__':
 
-    local_path = os.path.dirname(__file__)
-
-    # take a list of folder path 
-    args = [text_path, outfile_Path_Tfr]
+    main_root = os.path.normpath(r'D:\DATA\POLIMI\----TESI-----\NewCorpus')
+    index_file_path =  os.path.join(main_root+'\AllData.txt')
+    out_text_feature_path = os.path.join(main_root+'\Featurestext') 
+    
+    #READ THE FILE AND BUILD ARRAYS
+    #Audio File Names
+    with open(index_file_path, 'r') as AllDatafile:
+        X = [line.strip() for line in AllDatafile] 
+        arrayFileName = [line.split(';')[0] for line in X] 
+    AllDatafile.close()
+    #Transcriptions   
+    with open(index_file_path,  encoding='utf-8') as AllDatafile:
+        Z = [line.strip() for line in AllDatafile] 
+        arrayText = [line.split(';')[2] for line in Z]
+    AllDatafile.close()
 
     counter = 0
-    notf = []
-
-    model = gensim.models.Word2Vec.load(local_path + '/W2V_model/glove_WIKI')
-    files = glob.glob(args[0])
-    writer = tf.python_io.TFRecordWriter(args[3])
+    model = gensim.models.Word2Vec.load(main_root + '/W2V_model/glove_WIKI')
 
     # iterate over the list getting each file
-
-    for file in files:
-        # open the file and then call .read() to get the text
-        with open(file, encoding='utf-8') as f:
-            (one_hot_lab, label) = make_label(file)
-            print('procesing file name:', file, 'Label:', label, 'One Hot Repr:', one_hot_lab)
-            text = f.read().split()
-            text_byn, oh_lab_byn, nf, cnt = get_text_bynary(text, one_hot_lab, model)
-            counter += cnt
-            if nf:
-                notf.append(nf)
-            if len(text_byn) != 0:
-                write_to_tfrecord(oh_lab_byn, text_byn, writer)
-    writer.close()
-
-    print("tot word:", counter)
-    print("words not found:", len(notf))
-    print(notf)
+    i = 0
+    while i < len(arrayFileName):
+        current_text = arrayText[i]
+        text_byn, nf, cnt = get_text_bynary(current_text, model)
+        print(text_byn)
+        i += 1
+        
+    print('END')   
