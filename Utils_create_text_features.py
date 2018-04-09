05458469cb2ef3b2da4cd557018742f739b5e020
MIZAR\JORIGGI00 <juan-manuel.origgi@pwc.com>
2018-04-09 17:41:09 +0200

    commit

--- Utils_create_text_features.py
+++ Utils_create_text_features.py
@@ -4,6 +4,7 @@ import sys
 import gensim
 import numpy as np
 import tensorflow as tf
+import string
 
 
 def write_to_tfrecord(label, binary_text, writer):
@@ -15,30 +16,39 @@ def write_to_tfrecord(label, binary_text, writer):
     writer.write(example.SerializeToString())
 
 
-def get_text_bynary(text, model):
+def get_text_bynary(text, model, notFoundCounter):
     
-    print('get_text_bynary')
     
-    counter = 0
-    translated_text = []
-    notfounded = []
+    print(text)
+    
+    encodedText = []
+    notFoundedWord = []
 
     for word in text:
+        #Remove punctuation
+        word = ''.join(e for e in word if e.isalnum())
         print(word)
         try:
-            translated_text.append(model.wv[word])
+            encodedText.append(model.wv[word])
         except KeyError:
-            translated_text.append(model.wv['unknown'])
-            notfounded.append(word)
-
-    #text = np.array(translated_text, np.float32)
-    #return text.tobytes(), notfounded, counter
-    return text, notfounded, counter
+            print('NOT FOUND')
+            encodedText.append(model.wv['unknown'])
+            notFoundedWord.append(word)
+    
+    print('translated_text shape: ',np.asarray(encodedText).shape)
+    #TRANFORM ENCODED TEXT
+    encodedText = np.array(encodedText, np.float32)
+    encodedText = encodedText.tobytes()
+    
+    
+    return encodedText, notFoundCounter, notFoundedWord
 
 if __name__ == '__main__':
 
+    #SET ROOTS
     #main_root = os.path.normpath(r'D:\DATA\POLIMI\----TESI-----\NewCorpus')
     main_root = os.path.normpath(r'C:\Users\JORIGGI00\Documents\MyDOCs\Corpus_Test_Training')
+    modelPath = os.path.join(main_root+'\W2V_model\glove_WIKI')
     index_file_path =  os.path.join(main_root+'\AllData.txt')
     out_text_feature_path = os.path.join(main_root+'\FeaturesText') 
     
@@ -54,18 +64,23 @@ if __name__ == '__main__':
         arrayText = [line.split(';')[2] for line in Z]
     AllDatafile.close()
     
-    print(arrayText)
-    
-    counter = 0
-    modelPath = os.path.join(main_root+'\W2V_model\glove_WIKI')
+    notFoundCounter = 0
     model = gensim.models.Word2Vec.load(modelPath)#Se errore pip install gensim==3.0
 
-    # iterate over the list getting each file
-    ''''i = 0
+    #FOR EACH FILE ENCODE TEXT
+    i = 0
     while i < len(arrayFileName):
-        current_text = arrayText[i]
-        text_byn, nf, cnt = get_text_bynary(current_text, model)
-        print(text_byn)
+        print(arrayFileName[i])
+        
+        #Lower case
+        current_text = arrayText[i].lower()
+        #Isolate words
+        current_text = current_text.split()
+        
+        text_byn, notFoundCounter, notFoundedWord = get_text_bynary(current_text, model, notFoundCounter)
+
         i += 1
-    '''    
+    
+    print('notFoundCounter: ', notFoundCounter)
+       
     print('END')   
