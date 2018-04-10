bf8b485bb637262b61b9edbd4c18c375ab797d2a
MIZAR\JORIGGI00 <juan-manuel.origgi@pwc.com>
2018-04-10 13:36:56 +0200

    2

--- Train_sound.py
+++ Train_sound.py
@@ -7,14 +7,13 @@ from keras.layers import Dense
 from keras.layers import Bidirectional
 
 
-def readFeature():
-    mainRoot = os.path.normpath(r'D:\DATA\POLIMI\----TESI-----\NewCorpus\FeaturesAudio')
-    listA = [ item for item in os.listdir(mainRoot) if os.path.isfile(os.path.join(mainRoot, item)) ]
+def readFeatures(DirRoot):
+    listA = [ item for item in os.listdir(DirRoot) if os.path.isfile(os.path.join(DirRoot, item)) ]
     allFileFeature = []
     
     #READ encoded audio Features
     for file in listA:
-        datareader = csv.reader(open(os.path.join(mainRoot,file), 'r'))
+        datareader = csv.reader(open(os.path.join(DirRoot, file), 'r'))
         data = []
         for row in datareader:
             data.append([float(val) for val in row])
@@ -26,26 +25,6 @@ def readFeature():
     allFileFeature = np.asarray(allFileFeature)
     
     return allFileFeature 
-
-
-def readLabels():
-    mainRoot2 = os.path.normpath(r'D:\DATA\POLIMI\----TESI-----\NewCorpus\LablesEmotion')
-    listE = [ item for item in os.listdir(mainRoot2) if os.path.isfile(os.path.join(mainRoot2, item)) ]
-    allLabels = []
-    
-    #READ encoded emotion: Read the content as an array of numbers and not string as default
-    for file in listE:
-        datareader = csv.reader(open(os.path.join(mainRoot2,file), 'r'))
-        data = []
-        for row in datareader:
-            data.append([float(val) for val in row])
-        Y = np.array([np.array(xi) for xi in data])
-        allLabels.append(Y)
-        #print(Y.shape)
-        
-    allLabels = np.asarray(allLabels)
-    
-    return allLabels
 
 
 def getListshape(X):
@@ -68,33 +47,69 @@ def buildBLTSM():
     model.compile(loss='mean_squared_error', optimizer='adam', metrics=['acc'])
     return model
         
+
+def trainBLSTM(Features, Labels, model, limit):
     
-if __name__ == '__main__':
-    
-    #EXTRACT FEATURES AND LABELS
-    allFileFeature = readFeature()
-    allLabels = readLabels()
-    print(allFileFeature.shape)
-    print(allLabels.shape)
-    
-    #DEFINE LSTM MODEL
-    model = buildBLTSM()
-    
-    #TRAIN LSTM: considering totFile one at time
-    totFile = 10
-    for i in range(totFile):
+    for i in range(limit):
         #Format correctly single input and output
-        X, Y = reshapeLSTMInOut(allFileFeature[i], allLabels[i])
+        X, Y = reshapeLSTMInOut(Features[i], Labels[i])
         
         #FIT MODEL for one epoch on this sequence
         model.fit(X, Y, epochs=20, batch_size=1, verbose=2)
+    
+    return model  
+
+def trainBLSTMV2(Features, Labels, model, limit):
+    
+    for i in range(limit):
+        #Format correctly single input and output
+        X, Y = reshapeLSTMInOut(Features[i], Labels[i])
         
-        #counter += 1
+        #FIT MODEL for one epoch on this sequence
+        model.fit(X, Y, epochs=20, batch_size=1, verbose=2)
+    
+    return model     
+
+    
+if __name__ == '__main__':
+    
+    mainRoot = os.path.normpath(r'C:\Users\JORIGGI00\Documents\MyDOCs\Corpus_Test_Training')
+    #mainRoot = os.path.normpath(r'D:\DATA\POLIMI\----TESI-----\NewCorpus')
+    
+    #BUILD PATH FOR EACH FEATURE DIR
+    dirAudio = os.path.join(mainRoot + '\FeaturesAudio')
+    dirText = os.path.join(mainRoot + '\FeaturesText')
+    dirLabel = os.path.join(mainRoot + '\LablesEmotion')
+    
+    #EXTRACT FEATURES AND LABELS
+    allAudioFeature = readFeatures(dirAudio)
+    allTextFeature = readFeatures(dirText)
+    allLabels = readFeatures(dirLabel)
+    print(allAudioFeature.shape)
+    print(allTextFeature.shape)
+    print(allLabels.shape)
+    
+    #DEFINE BLSTM MODEL Parameters
+    modelType = 0 #1=OnlyAudio, 2=OnlyText, 3=Audio&Text
+    limit = 5 #number of file trained: len(allAudioFeature) or a number
+    
+    modelA = buildBLTSM()
+    modelT = buildBLTSM()
+    
+    #TRAIN LSTM: considering one at time
+    if modelType == 0:
+        modelA = trainBLSTM(allAudioFeature, allLabels, modelA, limit)
+    if modelType == 1:
+        modelT = trainBLSTM(allTextFeature, allLabels, modelT, limit)       
+    
+    #SAVE MODEL
+    modelPathAudio = os.path.normpath(mainRoot + '\RNN_Model_AUDIO_saved.h5')
+    modelA.save(modelPathAudio, overwrite=True)
     
     #EVALUATE LSTM
-    X, Y = reshapeLSTMInOut(allFileFeature[2], allLabels[2])
-    yhat = model.predict(X, verbose=0)
-    #yhat = model.predict_classes(X, verbose=0)
+    X, Y = reshapeLSTMInOut(allAudioFeature[2], allLabels[2])
+    #yhat = model.predict(X, verbose=0)
+    yhat = modelA.predict_classes(X, verbose=0)
     print('Expected:', Y, 'Predicted', yhat)
     
     
