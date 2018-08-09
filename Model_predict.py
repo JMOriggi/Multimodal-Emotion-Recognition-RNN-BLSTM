5f1b70a9cd0ffe6790bb31389ad6270355630d8d
Juan Manuel <Juan Manuel@DESKTOP-2FPEPMP.station>
2018-08-09 14:17:05 +0200

    c

--- Model_predict.py
+++ Model_predict.py
@@ -31,7 +31,7 @@ dirText = os.path.join(mainRoot + '\FeaturesText')
 #Model paths
 mainRootModelAudio = os.path.normpath(dirRes + '\RNN_Model_AUDIO_saved.h5')
 mainRootModelText = os.path.normpath(dirRes + '\RNN_Model_TEXT_saved.h5')
-OutputWeightsPathAudio = os.path.join(dirRes, 'weights-improvement-89-0.64.hdf5')
+OutputWeightsPathAudio = os.path.join(dirRes, 'weights-improvement-52-0.73.hdf5')
 OutputWeightsPathText = os.path.join(dirRes, 'weightsT-improvement-71-0.65.hdf5') 
 
 # --------------------------------------------------------------------------- #
@@ -42,7 +42,7 @@ flagLoadModelAudio = 1 #0=model, 1=weight
 flagLoadModelText = 0 #0=model, 1=weight
 labelLimit = 384 #170 for balanced, 384 for max [joy 299, ang 170, sad 245, neu 384] TOT 1098
 allfile = 1098
-nameFileResult = 'PredW_e89-'+str(modelType)+'-'+'Label_'+str(labelLimit)
+nameFileResult = 'PredW52-'+str(modelType)+'-'+'Label_'+str(labelLimit)
 #Max timestep used for padding, setted according to the training model (NOT MODIFY)
 maxTimestepAudio = 290
 maxTimestepText = 85
