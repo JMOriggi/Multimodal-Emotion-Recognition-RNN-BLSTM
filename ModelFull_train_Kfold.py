64d4fa360c196b642b93cc3a8b50339c05673d35
Juan Manuel <Juan Manuel@DESKTOP-2FPEPMP.fritz.box>
2018-12-30 11:56:40 +0100

    2

--- ModelFull_train_Kfold.py
+++ ModelFull_train_Kfold.py
@@ -38,7 +38,7 @@ modelPath = os.path.normpath(dirRes + '\RNN_Model_FULL_saved.h5')
 # --------------------------------------------------------------------------- #
 nb_lstm_cells = 128 #64 #128
 nb_classes = 4
-nb_hidden_units = 512 #128 #512
+nb_hidden_units = 256 #128 #512
 labelLimit = 1040 #1040 for balanced, 1708 for max [joy 1041, ang 1103, sad 1084, neu 1708] TOT 4936
 n_epoch = 35 #number of epoch 
 batchSize= 20
